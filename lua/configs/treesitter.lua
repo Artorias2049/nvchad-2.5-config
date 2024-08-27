28d5a9608ac0457d8ea13819e6c991c8120d7ffb
Arvind R <avi.x2077@gmail.com>
2024-08-27 16:55:10 +0530

    added files configured for python

---
+++ lua/configs/treesitter.lua
@@ -0,0 +1,25 @@
+local options = {
+    ensure_installed = {
+        "python",
+        "javascript",
+        "typescript",
+        "bash",
+        "lua",
+        "luadoc",
+        "markdown",
+        "printf",
+        "toml",
+        "vim",
+        "vimdoc",
+        "yaml",
+    },
+
+    highlight = {
+        enable = true,
+        use_languagetree = true,
+    },
+
+    indent = { enable = true },
+}
+
+require("nvim-treesitter.configs").setup(options)
